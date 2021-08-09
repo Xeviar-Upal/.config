7eee577719fd1e7e0dfb137beb0e77468ee95c55
Upal_Barua <56833544+UpalBarua@users.noreply.github.com>
2021-08-09 16:16:05 +0600

    Add files via upload

---
+++ pkglists/manjaro_pkglist.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+yay -S alacritty brave-bin cmatrix cowsay figlet intel-ucode kvantum-qt5 nerd-fonts-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji openbangla-keyboard otf-san-francisco papirus-icon-theme pfetch-git plasma5-applets-virtual-desktop-bar-git python-pip qt5-virtualkeyboard rhythmbox stacer timeshift tlp transmission-qt ttf-dejavu ttf-font-awesome ttf-ms-fonts ttf-roboto ttf-rubik ttf-ubuntu-font-family vim-plug vlc vscodium-bin youtube-dl
